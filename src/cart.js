99b44eb0239bde45d0d2d085d6951d1ff2aece40
Samie <samieoseh@gmail.com>
2025-10-30 11:57:42 +0100

    render cart done

--- src/cart.js
+++ src/cart.js
@@ -1,3 +1,6 @@
+const appear = document.querySelector(".appear");
+const h3 = document.querySelector("h3");
+
 export const CART = [];
 
 export function addToCart(product) {
@@ -10,5 +13,26 @@ export function removeFromCart() {
 }
 
 function renderCart(CART) {
-  console.log({ CART });
+  h3.innerHTML = `Your Cart (${CART.length})`;
+
+  appear.innerHTML = "";
+
+  CART.forEach((item) => {
+    const div = document.createElement("div");
+    const h4 = document.createElement("h4");
+    const p = document.createElement("p");
+    const img = document.createElement("img");
+
+    p.innerHTML = item.price;
+    p.classList.add("cart-price");
+    img.src = item.image.thumbnail;
+
+    h4.innerHTML = item.name;
+
+    div.appendChild(h4);
+    div.appendChild(p);
+    div.appendChild(img);
+
+    appear.appendChild(div);
+  });
 }
